f79bee116939013b030e6b2fdfca67c525fd11dc
Dave Yarwood <dave.yarwood@gmail.com>
2018-05-25 22:35:17 -0400

    move (un)hexification fns to util ns

--- src/advent/2015/12/08/puzzle_1.clj
+++ src/advent/2015/12/08/puzzle_1.clj
@@ -1,25 +1,14 @@
 (ns advent.2015.12.08.puzzle-1
-  (:require [clojure.string :as str]
+  (:require [advent.util     :as util]
+            [clojure.string  :as str]
             [clojure.java.io :as io]))
-
-;; source: https://stackoverflow.com/a/10087740/2338327
-(defn hexify [s]
-  (apply str (map #(format "%02x" %) (.getBytes s "UTF-8"))))
-
-;; source: https://stackoverflow.com/a/10087740/2338327
-(defn unhexify [s]
-  (let [bytes (into-array Byte/TYPE
-                 (map (fn [[x y]]
-                        (unchecked-byte (Integer/parseInt (str x y) 16)))
-                      (partition 2 s)))]
-    (String. bytes "UTF-8")))
 
 (defn count-extra-chars
   [s]
   (let [total-chars  (count s)
         string-chars (-> s
                          (str/replace #"\\x([0-9a-f]{2})"
-                                      (fn [[_ match]] (unhexify match)))
+                                      (fn [[_ match]] (util/hex->utf8 match)))
                          (str/replace #"\\\"" "\"")
                          (str/replace #"\\\\" (str/re-quote-replacement "\\"))
                          count
--- src/advent/util.clj
+++ src/advent/util.clj
@@ -8,3 +8,27 @@
   (let [chr (.read rdr)]
     (if (>= chr 0)
       (cons (char chr) (lazy-seq (char-seq rdr))))))
+
+;; source: https://stackoverflow.com/a/10087740/2338327
+(defn utf8->hex
+  "Encodes a UTF-8 string into a string of hexadecimal digits, I think in
+   2-digit pairs?
+
+   e.g. (utf8->hex \"hello\") => \"68656c6c6f\"
+   h -> 68, e-> 65, l -> 6c, l -> 6c, o -> 6f"
+  [s]
+  (apply str (map #(format "%02x" %) (.getBytes s "UTF-8"))))
+
+;; source: https://stackoverflow.com/a/10087740/2338327
+(defn hex->utf8
+  "Encodes a string of 2-digit hexadecimal numbers into a UTF-8 string.
+
+   e.g. (hex->utf8 \"68656c6c6f\") => \"hello\"
+   68 -> h, 65 -> e, 6c -> l, 6c -> l, 6f -> o"
+  [s]
+  (let [bytes (into-array Byte/TYPE
+                 (map (fn [[x y]]
+                        (unchecked-byte (Integer/parseInt (str x y) 16)))
+                      (partition 2 s)))]
+    (String. bytes "UTF-8")))
+
