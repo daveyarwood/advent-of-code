7cc82cbba1c2b3d72084a52692e74def3d035d76
Dave Yarwood <dave.yarwood@gmail.com>
2023-12-04 00:30:20 -0500

    Skipping puzzle 2

---
+++ src/advent/2023/12/03/puzzle_2.clj
@@ -0,0 +1,14 @@
+(ns advent.2023.12.03.puzzle-2)
+
+(comment
+  "I'm just gonna skip this one. I don't know what it is these grid problems,
+   but they're confusing and exhausting to do. I had trouble with the
+   grid-related problems from prior years, too.
+
+   I managed to solve puzzle 1, but it took a long time because I had a subtle
+   edge case bug that wasn't tested by the example input, so that held me up
+   for a while.
+
+   Then I got to puzzle 2, and it was clear that I would need to pretty much
+   start over and take a different approach, and I wasn't having fun anymore
+   at that point, so I skipped it. Enough grid problems!")
